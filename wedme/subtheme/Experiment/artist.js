af34d496b4d60d07be0546e4842456b0ab98adb6
Max Resnick <maxongithub@fastmail.fm>
2013-06-29 19:07:36 -0400

    Partially working - loop not completing.

--- wedme/subtheme/Experiment/artist.js
+++ wedme/subtheme/Experiment/artist.js
@@ -36,6 +36,8 @@ $("#artist").autocomplete({
             $("#log").append(ui.item ? ui.item.id + ' ' + ui.item.label : '(nothing)');
             $("#song").attr('disabled', false);
             $("#song").autocomplete("option", "source");
+            getSongs();
+
 
         },
         change: function (event, ui) {
@@ -45,41 +47,56 @@ $("#artist").autocomplete({
              }
         }
 });
+var songsList = [];
+
+function getSongs() {
+    var numsongs = 2; //at least 2 runs.
+    var startindex = 0;
+    runonceloop: //<~~~~Referenced in question
+    for (var j = 0; j <= _numsongs;j++) {
+        console.log('numsongs  ' + numsongs);
+        (function(_startindex){
+        $.ajax({
+
+            url: "http://developer.echonest.com/api/v4/artist/songs",
+            dataType: "jsonp",
+            data: {
+                results: 100,
+                api_key: "KTPZMU3CDARU82WJO",
+                format: "jsonp",
+                name: $("#artist").val(),
+                start: _startindex
+
+            },
+
+            success: function (data) {
+                var songs = data.response.songs;
+                _numsongs = data.response.total; //modify exit condition
+                for (var i = 0; i < songs.length; i++) {
+                    songsList.push(songs[i].title);
+
+                }
+
+            }
+        });
+        })(startindex);
+        startindex+=100;
+        if (j <= numsongs) {
+            break
+        }
+    }
+};
+
 
 
 $("#song").autocomplete({
+        source: songsList,
 
-        source: function (request, response) {
 
-            $.ajax({
-                url: "http://developer.echonest.com/api/v4/artist/songs",
-                dataType: "jsonp",
-                data: {
-
-                    results: 5,
-                    api_key: "KTPZMU3CDARU82WJO",
-                    format: "jsonp",
-                    name: $("#artist").val(),
-
-                    /**artist: "radiohead",*/
-                    title: request.term
-                },
-                success: function (data) {
-                    response($.map(data.response.songs, function (item) {
-                        return {
-                            label: item.title,
-                            value: item.title,
-                            id: item.id
-                        }
-                    }));
-                }
-            });
-        },
         minLength: 3,
         select: function (event, ui) {
             $("#log").empty();
             $("#log").append(ui.item ? ui.item.id + ' ' + ui.item.label : '(nothing)');
-
         },
         change: function (event, ui) {
             if (!ui.item) {
@@ -87,5 +104,6 @@ $("#song").autocomplete({
              }
         }
 });
-});
+    });
+
 
