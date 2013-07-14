f865159774edea57a0e86f344500952da234c6d6
Max Resnick <maxongithub@fastmail.fm>
2013-07-14 19:38:21 -0400

    Now works for a single artist, changing artist doesn't re-run.

--- wedme/subtheme/Experiment/artist.js
+++ wedme/subtheme/Experiment/artist.js
@@ -6,8 +6,22 @@
  * To change this template use File | Settings | File Templates.
  */
 /** Baseline code from EchoNest Blog */
-$(function() {
+$(function () {
+//Song total Initialization
+    var songTotal;
+//List of Songs By Artist ordered by "hottttnesss"
+    var songsList = [];
+
+    //Split s.t. all items added have a comma separator.
+    function split ( val ) {
+        return val.split(/,\s*/);
+    }
+    function extractLast ( term ) {
+        return split( term).pop();
+    }
 $("#song").attr('disabled', true);
+
+//function looks up input and returns suggested artist. Upon completion builds array of songs by the artist.
 $("#artist").autocomplete({
         source: function (request, response) {
             $.ajax({
@@ -31,14 +45,15 @@ $("#artist").autocomplete({
             });
         },
         minLength: 3,
+
         select: function (event, ui) {
-            $("#log").empty();
-            $("#log").append(ui.item ? ui.item.id + ' ' + ui.item.label : '(nothing)');
+            var terms = split(this.value)
+            //$("#log").empty();
+            //$("#log").append(ui.item ? ui.item.id + ' ' + ui.item.label : '(nothing)');
+            //Unlock Song Field.
             $("#song").attr('disabled', false);
-            $("#song").autocomplete("option", "source");
-            var differ = jQuery.Deferred();
-            differ.resolve(function () {
-                $.ajax({
+            //Gets total # of songs first.
+            var getSongs = $.ajax({
                     url: "http://developer.echonest.com/api/v4/artist/songs",
                     dataType: "jsonp",
                     data: {
@@ -48,16 +63,13 @@ $("#artist").autocomplete({
                         name: $("#artist").val(),
                     },
                     success: function (data) {
-                        songTotal = data.response.total; //modify exit condition
+                        songTotal = data.response.total;
                         console.log('hello  ' + songTotal);
                     }
-                });
-                return songTotal
-            });
-
-            differ.done( function(songTotal) {
+            //Key to sequencing is actually calling 'done' since ajax is a promise method.
+            }).done(function() {
                 var startindex = 0;
-                for (var j = 0; j <= songTotal;j++) {
+                for (; startindex <= songTotal;) {
                     console.log('numsongs  ' + songTotal);
                     (function(_startindex){
                         $.ajax({
@@ -82,6 +94,8 @@ $("#artist").autocomplete({
                      startindex+=100;
                 }
         });
+
+            //$("#song").autocomplete("option", "source");
         },
         change: function (event, ui) {
 
@@ -90,55 +104,32 @@ $("#artist").autocomplete({
              }
         }
 });
-var songsList = [];
-
-function getSongs() {
-    var startindex = 0;
-    for (var j = 0; j <= songTotal;j++) {
-        console.log('numsongs  ' + songTotal);
-        (function(_startindex){
-        $.ajax({
-            url: "http://developer.echonest.com/api/v4/artist/songs",
-            dataType: "jsonp",
-            data: {
-                results: 100,
-                api_key: "KTPZMU3CDARU82WJO",
-                format: "jsonp",
-                name: $("#artist").val(),
-                start: _startindex
-
-            },
-
-            success: function (data) {
-                var songs = data.response.songs;
-                for (var i = 0; i < songs.length; i++) {
-                    songsList.push(songs[i].title);
-                }
-
-            }
-        });
-        })(startindex);
-        startindex+=100;
-    }
-};
-
-
-
 $("#song").autocomplete({
-        source: songsList,
-
-
-        minLength: 3,
+        source: function( request, response ) {
+                response($.ui.autocomplete.filter(
+                    songsList, extractLast (request.term)
+                ));
+        },
+        minLength: 0,
         select: function (event, ui) {
-            $("#log").empty();
-            $("#log").append(ui.item ? ui.item.id + ' ' + ui.item.label : '(nothing)');
+            var terms = split(this.value)
+            //remove the current input
+            terms.pop();
+            //add the selected value
+            terms.push( ui.item.value)
+            //add placeholder to get the comma-and-space at the end
+            terms.push("");
+            this.value = terms.join( ", ")
+            return false;
+            //$("#log").empty();
+            //$("#log").append(ui.item ? ui.item.id + ' ' + ui.item.label : '(nothing)');
         },
         change: function (event, ui) {
             if (!ui.item) {
                  $(this).val('');
              }
         }
-});
     });
 
+});
 
