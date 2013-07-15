26df1ef7cdf7dba2963a2190dc4b41070127d5d0
Max Resnick <maxongithub@fastmail.fm>
2013-07-14 23:43:08 -0400

    Initial commit for playlist form form model.

--- wedme/playlistform/models.py
+++ wedme/playlistform/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django import forms
 
 # Create your models here.
+
+
+class AddToPlaylist(forms.Form):
+    submitter_first_name = forms.CharField(max_length=100, required=True)
+    submitter_last_name = forms.CharField(max_length=200, required=True)
+    email = forms.EmailField(required=True)
+    artist = forms.CharField(required=True)
+    artist_id = forms.CharField()
+    songs = forms.CharField(required=True)
+    songs_ids = forms.CharField()
