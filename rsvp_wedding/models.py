b92effcf8627e9392e4a267efaa049aab91d377e
Max Resnick <maxongithub@fastmail.fm>
2013-12-08 15:06:31 -0500

    initial commit of user model.

--- rsvp_wedding/models.py
+++ rsvp_wedding/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class WeddingGuestModel(models.Model):
+    """
+    An abstract base class model that provides basic data for guests.
+    """
+    will_attend = models.BooleanField()
+    is_vegetarian = models.BooleanField()
+
+    class Meta:
+        abstract = True
+
+
+class TimeStampedModel(models.Model):
+    """
+    An abstract base class model that provides self-updating ``created`` and ``modified`` fields.
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class PrimaryGuest(WeddingGuestModel, TimeStampedModel):
+    user = models.OneToOneField("auth.User")
+    is_allowed_partner = models.BooleanField()
+
+
+class GuestPartner(WeddingGuestModel, TimeStampedModel):
+    partner = models.OneToOneField(PrimaryGuest, related_name="guest_primary")
