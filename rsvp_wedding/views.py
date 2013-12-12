3630f45432b2b1b2c55c8f3b4050b22d07c55199
Max Resnick <maxongithub@fastmail.fm>
2013-12-11 21:35:52 -0500

    blah

--- rsvp_wedding/views.py
+++ rsvp_wedding/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django_nopassword.forms import AuthenticationForm
+from django.views.generic.edit import FormView
+from django import forms
+
+
+class RsvpStartView(FormView):
+    template_name = 'login.html'
+    form_class = AuthenticationForm
+    success_url = '/email-on-the-way/'
+
+    def get_form(self, form_class):
+        form = form_class(**self.get_form_kwargs())
+        form.fields['username'] = AuthenticationForm.username()
+        return form
