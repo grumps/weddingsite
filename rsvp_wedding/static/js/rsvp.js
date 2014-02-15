b47ebd3fb8d612abed7b74cd0362513c0917e319
Max Resnick <maxongithub@fastmail.fm>
2014-02-15 15:01:58 -0500

    working JS.

--- rsvp_wedding/static/js/rsvp.js
+++ rsvp_wedding/static/js/rsvp.js
@@ -1,18 +1,17 @@
-//Fade in extra elements on Y condition
+//Fade in extras on Y condition
 $('#id_will_attend').on('change', function(e) {
     if ($(this).val() === "Y") {
         $('.collapse').collapse('show')
         }
     else if ($(this).val() === "N") {
         $('.collapse').collapse('hide')
+        //TODO scalable code needed.
         $('#id_is_vegetarian').val('N')
         $('#id_will_arrive_thursday').val('N')
         $('#id_will_stay_saturday').val('N')
     }});
-
 //Show extras in the event the user is updating.
 $(document).ready(function(){
-    "use strict";
     if($('#id_will_attend').val() === "Y"){
         $('.collapse').collapse('show')
     }
